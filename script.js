c2e2946145fd81c7f794f9ceed149c57afb466b8
AdilTehranli <adiljt@code.edu.az>
2023-06-06 00:12:31 +0400

    Created

---
+++ script.js
@@ -0,0 +1,23 @@
+function choose(){
+    let bmw=document.getElementById('bm');
+    let opel =document.getElementById('op');
+    
+        for(let i=0;i<value;i++){
+            console.log("name : ${bmw1}");
+        }
+    
+}
+let opel1={
+    model:'corsa',
+    color:'red',
+    speed:120
+};
+
+let bmw1={
+    model:'m5',
+    color:'white',
+    speed:240
+}; 
+
+choose.call(bmw1)
+choose.call(opel1)
